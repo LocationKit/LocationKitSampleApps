f2f3a8c21a7cb9c448a15cd9addd81c41fabb044
Victor Quinn <mail@victorquinn.com>
2015-11-20 16:03:04 -0500

    Finish CurrentPlace example app

--- CurrentPlace/CurrentPlace/ViewController.swift
+++ CurrentPlace/CurrentPlace/ViewController.swift
@@ -12,9 +12,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let locationManager = LKLocationManager()
+
+    @IBOutlet weak var placeLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Start LocationKit
+        self.placeLabel.text = "Place undetermined"
+
+        #if DEBUG
+        locationManager.debug = true
+        #endif
+        locationManager.apiToken = "your_api_token_here"
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func whereAmI(sender: UIButton) {
+        locationManager.requestPlace { (place: LKPlacemark?, error: NSError?) -> Void in
+            if let place = place {
+                self.placeLabel.text = "\(place.subThoroughfare!) \(place.thoroughfare!), \(place.locality!), \(place.administrativeArea!)"
+            } else if error != nil {
+                print("Uh oh, got an error: \(error)")
+            } else {
+                self.placeLabel.text = "Place could not be determined"
+            }
+        }
     }
 }
