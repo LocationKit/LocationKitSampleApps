02d464f010639d50c14e892cbf97b7aed3ae90b8
Victor Quinn <mail@victorquinn.com>
2015-11-20 10:48:39 -0500

    Add SingleLocation with button

---
+++ SingleLocation/SingleLocation/SingleLocation-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Banana.h
+//  SingleLocation
+//
+//  Created by Victor Quinn on 11/19/15.
+//  Copyright © 2015 LocationKit. All rights reserved.
+//
+
+#import <LocationKit/LocationKit.h>
--- SingleLocation/SingleLocation/ViewController.swift
+++ SingleLocation/SingleLocation/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Instantiate LocationKit and keep an instance around to use in this file
+    let locationManager = LKLocationManager()
+    
+    @IBOutlet weak var currentLocationLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.currentLocationLabel.text = ""
+
+        // Enable debug mode in non-production builds so we can see what's going on
+        #if DEBUG
+            self.locationManager.debug = true
+        #endif
+
+        // Set your API token (retrieved from https://dashboard.locationkit.io)
+        self.locationManager.apiToken = "your_api_token_here"
+
+        // Start updating location
+        self.locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func requestLocationWithHandler(sender: UIButton) {
+        // Request the current location with a handler
+        self.locationManager.requestLocation { (location: CLLocation?, error: NSError?) -> Void in
+            // We have to make sure the location is set, could be nil
+            if let location = location {
+                // Let's set the label in the center of the screen
+                self.currentLocationLabel.text = "(\(location.coordinate.latitude), \(location.coordinate.longitude))"
+            } else {
+                print("Oops, something went wrong: \(error)")
+            }
+        }
     }
-
-
+    
+    
 }
 
